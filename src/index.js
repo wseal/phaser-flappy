d47cbd2f2d6347f31213230e2c3c6216901209a5
OWL-WIN <owl>
2025-11-28 10:49:52 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const config = {
     default: "arcade",
     arcade: {
       debug: true,
-      gravity: { y: 200 },
+      // gravity: { y: 200 },
     },
   },
 
@@ -25,15 +25,9 @@ let upperPipe = null;
 let lowerPipe = null;
 const flapVelocityY = 200;
 const initalPos = { x: config.width * 0.1, y: config.height / 2 };
+
+let pipHorizontalDistance = 10;
 const pipeVerticalDistanceRange = [150, 250];
-const pipVerticalDistance = Phaser.Math.Between(
-  pipeVerticalDistanceRange[0],
-  pipeVerticalDistanceRange[1]
-);
-const pipVerticalPos = Phaser.Math.Between(
-  20,
-  config.height - 20 - pipVerticalDistance
-);
 
 // load assets, such as images, music, animations ...
 function preload() {
@@ -44,19 +38,18 @@ function preload() {
 
 //
 function create() {
-  let sky = this.add.image(config.width / 2, config.height / 2, "sky");
-  sky.setOrigin(0.5, 0.5);
+  this.add.image(0, 0, "sky").setOrigin(0);
 
   bird = this.physics.add.sprite(initalPos.x, initalPos.y, "bird").setOrigin(0);
-
-  upperPipe = this.add.image(400, pipVerticalPos, "pipe").setOrigin(0, 1);
-  lowerPipe = this.add
-    .image(400, upperPipe.y + pipVerticalDistance, "pipe")
-    .setOrigin(0, 0);
-
   // bird.body.velocity.x = 200;
-  // bird.body.gravity.y = 100;
+  bird.body.gravity.y = 400;
   // debugger
+
+  for (let i = 0; i < 4; i++) {
+    upperPipe = this.physics.add.image(0, 0, "pipe").setOrigin(0, 1);
+    lowerPipe = this.physics.add.image(400, 0, "pipe").setOrigin(0, 0);
+    placePipe(upperPipe, lowerPipe);
+  }
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown-SPACE", flap);
@@ -72,6 +65,27 @@ function update(time, delta) {
   if (bird.y > config.height || bird.y < -bird.height / 2) {
     restartGame();
   }
+}
+
+function placePipe(uPipe, lPipe) {
+  pipHorizontalDistance += 400;
+  const pipVerticalDistance = Phaser.Math.Between(
+    pipeVerticalDistanceRange[0],
+    pipeVerticalDistanceRange[1]
+  );
+  const pipVerticalPos = Phaser.Math.Between(
+    20,
+    config.height - 20 - pipVerticalDistance
+  );
+
+  uPipe.x = pipHorizontalDistance;
+  uPipe.y = pipVerticalPos;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipVerticalDistance;
+
+  lPipe.body.velocity.x = -200;
+  uPipe.body.velocity.x = -200;
 }
 
 function flap() {
