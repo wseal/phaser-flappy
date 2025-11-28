48a1e13b6ae83c68cef42e85252f22b85f50749e
OWL-WIN <owl>
2025-11-28 09:26:47 +0800

    velocity y

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const config = {
     default: "arcade",
     arcade: {
       debug: true,
+      gravity: { y: 200 },
     },
   },
 
@@ -26,27 +27,45 @@ function preload() {
 }
 
 let bird = null;
+const flapVelocityY = 200;
+const initalPos = { x: config.width * 0.1, y: config.height / 2 };
 //
 function create() {
-  let sky = this.add.image(config.width / 2, config.height * 0.5, "sky");
+  let sky = this.add.image(initalPos.x, initalPos.y, "sky");
   sky.setOrigin(0.5, 0.5);
 
   bird = this.physics.add
     .sprite(config.width / 3, config.height / 4, "bird")
     .setOrigin(0);
 
-  bird.body.velocity.x = 200;
+  // bird.body.velocity.x = 200;
   // bird.body.gravity.y = 100;
   // debugger
+
+  this.input.on("pointerdown", flap);
+  this.input.keyboard.on("keydown-SPACE", flap);
 }
 
 function update(time, delta) {
   // console.log("hello");
-  if (bird.x > config.width - bird.width) {
-    bird.body.velocity.x = -200;
-  } else if (bird.x < 0) {
-    bird.body.velocity.x = 200;
+  // if (bird.x > config.width - bird.width) {
+  //   bird.body.velocity.x = -200;
+  // } else if (bird.x < 0) {
+  //   bird.body.velocity.x = 200;
+  // }
+  if (bird.y > config.height || bird.y < -bird.height / 2) {
+    restartGame();
   }
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocityY;
+}
+
+function restartGame() {
+  bird.x = initalPos.x;
+  bird.y = initalPos.y;
+  bird.body.velocity.y = 0;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
